748a8ea1e4bc40f581aeef9b0f224a34ade8d403
John Collins <john.collins@students.uni-mannheim.de>
2024-02-28 16:18:02 +0100

    init

---
+++ Demo.R
@@ -0,0 +1,7 @@
+data_directory <- '2024-ASADataFest-Data-and-Documentation/2024-ASADataFest-Data-and-Documentation/'
+
+checkpoints <- read.csv(paste(data_directory,'Data Files/checkpoints.csv'))
+items <- read.csv(paste(data_directory,'Data Files/items.csv'))
+media_views <- read.csv(paste(data_directory,'Data Files/media_views.csv'))
+page_views <- read.csv(paste(data_directory,'Data Files/page_views.csv'))
+responses <- read.csv(paste(data_directory,'Data Files/responses.csv'))
